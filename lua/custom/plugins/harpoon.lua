7efdac92ad6b6685733fdda48ba1c03ec81f2dd8
SaxsCode <maxsax.sch@gmail.com>
2025-05-18 21:05:15 +0200

    fix(harpoon): use add instead of append

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,44 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup {
+      default = {
+        sync_on_ui_close = true,
+      },
+    }
+
+    -- Key mappings
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end, { desc = 'Add file to Harpoon' })
+
+    vim.keymap.set('n', '<leader>hh', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Toggle Harpoon menu' })
+
+    -- Quick navigation to files 1-4
+    vim.keymap.set('n', '<leader>1', function()
+      harpoon:list():select(1)
+    end, { desc = 'Go to Harpoon file 1' })
+    vim.keymap.set('n', '<leader>2', function()
+      harpoon:list():select(2)
+    end, { desc = 'Go to Harpoon file 2' })
+    vim.keymap.set('n', '<leader>3', function()
+      harpoon:list():select(3)
+    end, { desc = 'Go to Harpoon file 3' })
+    vim.keymap.set('n', '<leader>4', function()
+      harpoon:list():select(4)
+    end, { desc = 'Go to Harpoon file 4' })
+
+    -- Next/previous navigation
+    vim.keymap.set('n', '<leader>n', function()
+      harpoon:list():next()
+    end, { desc = 'Next Harpoon file' })
+    vim.keymap.set('n', '<leader>p', function()
+      harpoon:list():prev()
+    end, { desc = 'Previous Harpoon file' })
+  end,
+}
