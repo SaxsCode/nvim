35065ff1926c468e425037a98359bcb47adebbec
SaxsCode <maxsax.sch@gmail.com>
2025-05-18 16:09:09 +0200

    feat: setup keymaps to toggle term'

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,3 +1,14 @@
 return {
-  { 'akinsho/toggleterm.nvim', version = '*', config = true },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      open_mapping = [[<C-\>]], -- Open/close terminal with Ctrl+\
+    },
+    config = function()
+      require('toggleterm').setup {
+        open_mapping = [[<C-\>]],
+      }
+    end,
+  },
 }
