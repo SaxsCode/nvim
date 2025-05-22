a91c463d83764d7d51fc81a04b1404bd60ac50e2
SaxsCode <maxsax.sch@gmail.com>
2025-05-22 22:28:21 +0200

    feat: add lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,6 +2,15 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup()
+    require('lualine').setup {
+      options = {
+        icons_enabled = true,
+      },
+      sections = {
+        lualine_b = { 'branch', 'diff' },
+        lualine_x = {},
+        lualine_y = { 'filetype' },
+      },
+    }
   end,
 }
