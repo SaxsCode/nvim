7f0d15f9ec45bc1ce03219f3dcbd4b1dee42ed0b
Sax Arch <maxsax.sch@gmail.com>
2025-06-04 18:31:10 +0200

    fix: solve syntax error in init lua

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ end)
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.opt.expendtab = true,
+vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.breakindent = true
 
