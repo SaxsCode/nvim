9a43bbf394bdae4e5100f54040f304299ba58822
Sax Arch <maxsax.sch@gmail.com>
2025-06-07 12:40:00 +0200

    feat: do not use shift keys for compiler shortcuts

--- init.lua
+++ init.lua
@@ -669,15 +669,17 @@ require('lazy').setup({
   },
 })
 
-
 -- Open compiler
-vim.api.nvim_set_keymap('n', '<F6>', "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F6>', '<cmd>CompilerOpen<cr>', { noremap = true, silent = true })
 
 -- Redo last selected option
-vim.api.nvim_set_keymap('n', '<S-F6>',
-     "<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
-  .. "<cmd>CompilerRedo<cr>",
- { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  'n',
+  '<F7>',
+  '<cmd>CompilerStop<cr>' -- (Optional, to dispose all tasks before redo)
+    .. '<cmd>CompilerRedo<cr>',
+  { noremap = true, silent = true }
+)
 
 -- Toggle compiler results
-vim.api.nvim_set_keymap('n', '<S-F7>', "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F8>', '<cmd>CompilerToggleResults<cr>', { noremap = true, silent = true })
