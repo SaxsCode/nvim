e02c5caf237eb1ba4e8463e33ef9eeb95fcd96bc
SaxsCode <maxsax.sch@gmail.com>
2025-06-18 23:17:03 +0200

    fix: treesitter-context works correctly now

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -1,10 +1,17 @@
 return {
-  'nvim-treesitter/nvim-treesitter-context',
-  lazy = false,
-  opts = {},
-  config = function()
-    vim.keymap.set('n', '[c', function()
-      require('treesitter-context').go_to_context(vim.v.count1)
-    end, { silent = true })
-  end,
-}
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      enable = true,
+      max_lines = 0,
+      mode = 'cursor',
+      line_numbers = true,
+    },
+    config = function()
+      require('treesitter-context').setup{
+        enable = true,
+        max_lines = 0,
+        mode = 'cursor',
+        line_numbers = true,
+      }
+    end,
+  }
