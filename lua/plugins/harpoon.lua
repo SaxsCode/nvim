78f48e06bed7cc46f3a13d347aaaef14541c9ba2
SaxsCode <maxsax.sch@gmail.com>
2025-09-11 20:35:01 +0200

    feat: huge improvement on harpoon keys

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -20,25 +20,18 @@ return {
     end, { desc = 'Toggle Harpoon menu' })
 
     -- Quick navigation to files 1-4
-    vim.keymap.set('n', '<leader>h1', function()
+    vim.keymap.set('n', '<leader>n', function()
       harpoon:list():select(1)
     end, { desc = 'Go to Harpoon file 1' })
-    vim.keymap.set('n', '<leader>h2', function()
+    vim.keymap.set('n', '<leader>e', function()
       harpoon:list():select(2)
     end, { desc = 'Go to Harpoon file 2' })
-    vim.keymap.set('n', '<leader>h3', function()
+    vim.keymap.set('n', '<leader>i', function()
       harpoon:list():select(3)
     end, { desc = 'Go to Harpoon file 3' })
-    vim.keymap.set('n', '<leader>h4', function()
+    vim.keymap.set('n', '<leader>o', function()
       harpoon:list():select(4)
     end, { desc = 'Go to Harpoon file 4' })
 
-    -- Next/previous navigation
-    vim.keymap.set('n', '<leader>hn', function()
-      harpoon:list():next()
-    end, { desc = 'Next Harpoon file' })
-    vim.keymap.set('n', '<leader>hp', function()
-      harpoon:list():prev()
-    end, { desc = 'Previous Harpoon file' })
   end,
 }
